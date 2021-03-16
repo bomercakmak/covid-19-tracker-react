3e58442691613a84db0af864b5ecddaf23362caa
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-03-16 15:20:12 +0100

    I created components structure.I also added module.css files for each components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import React, { Component } from 'react'
+import { Cards, Chart,CountryPicker} from './components'
+import styles from './App.module.css'
 
-function App() {
-  return (
-    <div >
-      <h1>Hello</h1>
-    </div>
-  );
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>App</h1>
+        <Cards></Cards>
+        <Chart></Chart>
+        <CountryPicker></CountryPicker>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
