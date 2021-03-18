2b37aed63c02896aa978a949287fc5b0e99a4de5
Bahadir Omer Cakmak <bahadir0646@gmail.com>
2021-03-18 14:15:17 +0100

    I created chart and I give some basic styles. I also added CountryPicker.

--- src/App.js
+++ src/App.js
@@ -19,8 +19,9 @@ class App extends Component {
     return (
       <div className={styles.container}>
         <Cards data={data}/>
-        <Chart/>
         <CountryPicker/>
+        
+        <Chart/>
       </div>
     );
   }
--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -5,7 +5,7 @@ import{Line,Bar} from 'react-chartjs-2'
 import styles from './Chart.module.css'
 
 const Chart = () => {
-    const [dailyData,setDailyData] = useState({});
+    const [dailyData,setDailyData] = useState([]);
     
     useEffect(() => {
         const fetchAPI = async () => {
@@ -16,17 +16,30 @@ const Chart = () => {
     })
 
     const lineChart = (
-       dailyData ?  
+       dailyData.length ?  
         (<Line
             data={{
-                labels:'',
-                datasets:[{},{}]
+                labels:dailyData.map(({date}) => date),
+                datasets:[{
+                    data:dailyData.map(({confirmed}) => confirmed),
+                    label:"Infected",
+                    borderColor:'#3333ff',
+                    fill:true,
+                },{
+                    data:dailyData.map(({deaths}) => deaths),
+                    label:"Deaths",
+                    borderColor:'red',
+                    backgroundColor:'rgba(255,0,0,0.5)',
+                    fill:true,
+                }]
             }}
         />) : null
     )
 
     return (
-        <h1>Chart</h1>
+        <div className={styles.container}>
+            {lineChart}
+        </div>
     )
 }
 
